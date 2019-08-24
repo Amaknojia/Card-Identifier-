ef94fb05532395679240f4597e5570ad7867bb49
Amaknojia <53323277+Amaknojia@users.noreply.github.com>
2019-08-24 14:50:14 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace card_identifier
+{
+    partial class frmCardIdentifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.lblDisplayCardName = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::card_identifier.Properties.Resources.Queen_Hearts;
+            this.pictureBox5.Location = new System.Drawing.Point(666, 81);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(151, 191);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::card_identifier.Properties.Resources.King_Diamonds;
+            this.pictureBox4.Location = new System.Drawing.Point(509, 81);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(151, 191);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::card_identifier.Properties.Resources._10_Diamonds;
+            this.pictureBox3.Location = new System.Drawing.Point(352, 81);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(151, 191);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::card_identifier.Properties.Resources._8_Hearts;
+            this.pictureBox2.Location = new System.Drawing.Point(195, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(151, 191);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::card_identifier.Properties.Resources.Ace_Hearts;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 191);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(280, 28);
+            this.lblHeading.MaximumSize = new System.Drawing.Size(100, 100);
+            this.lblHeading.MinimumSize = new System.Drawing.Size(300, 40);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(300, 40);
+            this.lblHeading.TabIndex = 50;
+            this.lblHeading.Text = "Click a card to see its name";
+            // 
+            // lblDisplayCardName
+            // 
+            this.lblDisplayCardName.AutoSize = true;
+            this.lblDisplayCardName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayCardName.Location = new System.Drawing.Point(326, 295);
+            this.lblDisplayCardName.MaximumSize = new System.Drawing.Size(100, 100);
+            this.lblDisplayCardName.MinimumSize = new System.Drawing.Size(300, 40);
+            this.lblDisplayCardName.Name = "lblDisplayCardName";
+            this.lblDisplayCardName.Size = new System.Drawing.Size(300, 40);
+            this.lblDisplayCardName.TabIndex = 51;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(390, 385);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(132, 38);
+            this.btnExit.TabIndex = 54;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmCardIdentifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblDisplayCardName);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmCardIdentifier";
+            this.Text = "Card Identifier";
+            this.Load += new System.EventHandler(this.frmCardIdentifier_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label lblDisplayCardName;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace card_identifier
+{
+    public partial class frmCardIdentifier : Form
+    {
+        public frmCardIdentifier()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            lblDisplayCardName.Text = "King of Diamonds";
+        }
+
+        private void frmCardIdentifier_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            lblDisplayCardName.Text = "Ace of Hearts"; 
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            lblDisplayCardName.Text = "Eight of Hearts";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            lblDisplayCardName.Text = "Ten of Diamonds";
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            lblDisplayCardName.Text = "Queen of Hearts";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
